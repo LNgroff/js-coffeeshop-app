c5b9c3191bbed0b1850ee02f328dac8aafe3adda
LNgroff <LNgroff@gmail.com>
2020-10-29 21:13:31 +0000

    finished add-to-order and place-order. Lab complete
    Co-authored-by: cvang1231 <vangchandini@gmail.com>

--- static/js/coffeeShop.js
+++ static/js/coffeeShop.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// Adds item to cart by name
 const addItemToCart = (itemName) => {
   $('#cart-items').append(`
     <tr>
@@ -7,12 +8,13 @@ const addItemToCart = (itemName) => {
     </tr>
   `);
 };
-
+// Empties items and sets total to $0.00
 const resetCart = () => {
   $('#cart-total').html('0.00');
   $('#cart-items').empty();
 };
 
+// Increases the total of cart price after each item is added
 const incrementCartTotal = (price) => {
   const cartTotal = $('#cart-total');
 
@@ -22,6 +24,7 @@ const incrementCartTotal = (price) => {
   cartTotal.html(total.toFixed(2));
 };
 
+// Increases the quantity of coffee sold
 const incrementCoffeeSold = (amountSold) => {
   let coffeeSold = Number($('#coffee-sold-counter').html());
   coffeeSold += amountSold;
@@ -29,6 +32,7 @@ const incrementCoffeeSold = (amountSold) => {
   $('#coffee-sold-counter').html(coffeeSold);
 };
 
+// Displays the order status
 const setProgressAndStatus = (progressVal, statusMsg) => {
   $('#order-progress').attr('value', progressVal);
   $('#order-status-message').html(statusMsg);
@@ -38,3 +42,13 @@ const setProgressAndStatus = (progressVal, statusMsg) => {
 //
 // Add your event handlers below.
 //
+
+$('.add-to-order').on('click', () => {
+  addItemToCart('Coffee');
+  incrementCartTotal(1.50);
+});
+
+$('#place-order').on('click', () => {
+  incrementCoffeeSold($('#cart-items').children().length);
+  resetCart();
+})
